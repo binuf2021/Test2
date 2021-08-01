687ec475f1f964cab0c5e9ca88416111419fa496
binuf2021 <binuf.varghese07@gmail.com>
2021-08-01 19:07:12 +0530

    Added new code

--- excel.py
+++ excel.py
@@ -1,3 +1,9 @@
+So lets rock and role 
+Why it seems easy
+You are going to learn and learn hard
+so that you could answer most of the questions
+Thankyou
+
 Python for sales team
 Quickbooks 2021
 I going to create new python code
